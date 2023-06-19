3094e90a4cd5b5291341fb395b9f7a2672ffa10d
Laboratorio <laboratorio@udla-ec.int>
2023-06-19 16:01:01 -0500

    Creacion de codigo imprimiendo los valores del alumno

--- ESTRUCTURAS.c
+++ ESTRUCTURAS.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 
 int main()
-
-
 {
     struct Alumno
     {
@@ -12,49 +10,40 @@ int main()
         char carrera[100];
         int edad;
         float promedio;
+    } alumno[6];
 
-    }alumno[5];
-   
-
-    for (int i = 0; i < 5; i++)
+    for (int i = 1; i < 6; i++)
     {
-        printf ("Escriba el nombre del alumno %d\n", i);
-        gets (alumno[i].nombre);
-        
+        printf("Escriba el nombre del alumno %d:\n", i);
+        fflush(stdin);
+        fgets(alumno[i].nombre, sizeof(alumno[i].nombre), stdin);
 
-        puts ("Nombre introducido:");
-        puts (alumno[i].nombre);
+        printf("Escriba la direccion del alumno %d:\n", i);
+        fflush(stdin);
+        fgets(alumno[i].direccion, sizeof(alumno[i].direccion), stdin);
 
+        printf("Escriba la carrera del alumno %d:\n", i);
+        fflush(stdin);
+        fgets(alumno[i].carrera, sizeof(alumno[i].carrera), stdin);
 
-        printf ("Escriba la direccion del alumno %d\n", i);
-        gets (alumno[i].direccion);
+        printf("Escriba la edad del alumno %d:\n", i);
+        scanf("%d", &alumno[i].edad);
 
-        puts ("Direccion introducido:");
-        puts (alumno[i].direccion);
+        printf("Escriba el promedio del alumno %d:\n", i);
+        scanf("%f", &alumno[i].promedio);
 
-
-        printf ("Escriba la carrera del alumno %d\n", i);
-        gets (alumno[i].carrera);
-
-        puts ("Carrera introducido:");
-        puts (alumno[i].carrera);
-
-
-        printf ("Escriba la edad del alumno %d\n", i);
-        gets (alumno[i].edad);
-
-        puts ("Edad introducido:");
-        puts (alumno[i].edad);
-
-
-        printf ("Escriba el promedio del alumno %d\n", i);
-        gets (alumno[i].promedio);
-
-        puts ("Pomedio introducido:");
-        puts (alumno[i].promedio);
-        
+        fflush(stdin);
     }
-    
-    return 0;
 
+    for (int i = 1; i < 6; i++)
+    {
+        printf("\nDatos del alumno %d:\n", i);
+        printf("Nombre: %s", alumno[i].nombre);
+        printf("Direccion: %s", alumno[i].direccion);
+        printf("Carrera: %s", alumno[i].carrera);
+        printf("Edad: %d\n", alumno[i].edad);
+        printf("Promedio: %.2f\n", alumno[i].promedio);
+    }
+
+    return 0;
 }
